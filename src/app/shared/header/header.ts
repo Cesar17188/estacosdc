4eca932ee4069948d7313b0a516bff1f61316cac
Cesar <cesareli17188@gmail.com>
2026-04-04 12:20:16 -0500

    cambios al menu desplegable para cerrarlo y evitar desplazamiento con menu abierto

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Renderer2, OnDestroy, HostListener } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
@@ -9,9 +10,43 @@ import { CommonModule } from '@angular/common';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
+export class Header implements OnDestroy {
   isMenuOpen = false;
+
+constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (event.target.innerWidth > 768 && this.isMenuOpen) {
+      this.closeMenu();
+    }
+  }
+
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+    this.updateBodyScroll();
+  }
+
+  // Add this method to handle link clicks
+  closeMenu() {
+    this.isMenuOpen = false;
+    this.updateBodyScroll();
+  }
+
+   private updateBodyScroll() {
+    if (this.isMenuOpen) {
+      this.renderer.addClass(this.document.body, 'no-scroll');
+    } else {
+      this.renderer.removeClass(this.document.body, 'no-scroll');
+    }
+  }
+
+  // Ensure the class is removed if the user navigates away
+  // and the header gets destroyed while the menu is still open
+  ngOnDestroy() {
+    this.renderer.removeClass(this.document.body, 'no-scroll');
   }
 }
