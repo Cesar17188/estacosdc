817f092e5ce41bdefdd7872172b1adc1c33d031d
Cesar <cesareli17188@gmail.com>
2026-04-05 20:26:55 -0500

    componente history completado

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
 import { Hero } from './components/hero/hero';
+import { History } from "./components/history/history";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero],
+  imports: [RouterOutlet, Header, Hero, History],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
