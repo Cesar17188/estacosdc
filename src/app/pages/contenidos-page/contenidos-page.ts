ea079a78c165f2f2a256f6b4e518c56ee668a086
Cesar <cesareli17188@gmail.com>
2026-04-30 22:01:04 -0500

    mejorado los contenidos

--- src/app/pages/contenidos-page/contenidos-page.ts
+++ src/app/pages/contenidos-page/contenidos-page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,signal } from '@angular/core';
 import { Galeria } from '../../components/galeria/galeria';
 import { Cocteleria } from '../../components/cocteleria/cocteleria';
 import { Maridaje } from '../../components/maridaje/maridaje';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contenidos-page',
-  imports: [ Galeria, Cocteleria, Maridaje ],
+  imports: [ CommonModule, Galeria, Cocteleria, Maridaje ],
   templateUrl: './contenidos-page.html',
   styleUrl: './contenidos-page.scss',
 })
-export class ContenidosPage {}
+export class ContenidosPage {
+  // El estado de este componente padre se reduce únicamente a saber qué pestaña está activa
+  activeTab = signal<'mixologia' | 'maridaje' | 'galeria'>('mixologia');
+
+  setTab(tab: 'mixologia' | 'maridaje' | 'galeria') {
+    this.activeTab.set(tab);
+  }
+}
