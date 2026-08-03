ca4781977b23b98904b551b3e3f15ea33641e822
Cesar <cesareli17188@gmail.com>
2026-08-03 10:40:00 -0500

    link de visitas actualizado

--- src/app/components/visit/visit.ts
+++ src/app/components/visit/visit.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-visit',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './visit.html',
   styleUrl: './visit.scss',
 })
